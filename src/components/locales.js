31b59f4f60e3b01690b5816b8fd855f1adc09997
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-04-01 17:37:15 -0400

    added language toggle

--- src/components/locales.js
+++ src/components/locales.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Link } from "gatsby";
+
 export const DEFAULT_LOCALE = "en"; 
 export const ACCEPTED_LOCALES = ["en", "es"];
 
@@ -7,4 +10,15 @@ export function standardizeLocale(locale) {
 
 export function localizeRoute(route, locale) {
     return '/' + standardizeLocale(locale) + route;
+}
+
+// This implementation only works for when we have two locales, en and es:
+export const LanguageToggle = (props) => {
+    const alternateLocale = props.locale === 'en' ? 'es' : 'en';
+    return (
+        <Link className="nr-language-toggle has-text-black is-uppercase" to={'/' + alternateLocale}>
+            <span className={props.locale === "en" ? 'nr-is-underlined' : ''}>EN</span>
+            /
+            <span className={props.locale === "es" ? 'nr-is-underlined' : ''}>ESP</span>
+        </Link>)
 }
--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -5,16 +5,17 @@ import RentLogo from '../images/rent.svg';
 import Layout from '../components/layout';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
-import { localizeRoute } from '../components/locales';
+import { localizeRoute, LanguageToggle } from '../components/locales';
 
 export const LandingPageScaffolding = (props) => (
     <section className="hero is-fullheight">
+        <LanguageToggle locale={props.content.node_locale} />
         <div className="hero-body">
             <div className="container has-text-centered">
                 <h1 className="title has-text-black is-size-2 is-spaced is-uppercase has-text-weight-bold">
                     {props.content.title}
                 </h1>
-                <figure className="image is-3by1 rent-logo">
+                <figure className="image is-3by1 nr-rent-logo">
                     <img src={RentLogo} alt="rent-logo" />
                 </figure>
                 <div className="subtitle has-text-black is-size-5" >
@@ -26,7 +27,7 @@ export const LandingPageScaffolding = (props) => (
                 </Link>
             </div>
         </div>
-        <div className="container footer-link has-text-centered">
+        <div className="container nr-footer-link has-text-centered">
             <Link to={localizeRoute('/about',props.content.node_locale)}>{props.content.modalLinkCta}</Link>
         </div>
     </section>
