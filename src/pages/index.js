c9fe97d339d2ca8183ddd070b89ce6fab02881f5
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-04-01 18:21:28 -0400

    added more comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import { useEffect } from "react";
 import { navigate } from "gatsby";
 import { DEFAULT_LOCALE, standardizeLocale } from "../components/i18n";
 
+// This is a dummy component for the root of our site that redirects people
+// to a localized landing page based on their browser's preferred language
+// 
 // Adapted from this very useful StackOverflow post: 
 // https://stackoverflow.com/questions/59908989/redirect-based-on-browser-language-in-gatsby
 
