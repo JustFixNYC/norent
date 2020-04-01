db4db2c3aa0d705075882a8074a3e1675880eccb
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-04-01 17:07:45 -0400

    added styling for step 2 pages

--- src/components/city-page.js
+++ src/components/city-page.js
@@ -3,36 +3,35 @@ import React from 'react';
 import '../scss/style.scss';
 import RentLogo from '../images/rent.svg';
 import Layout from '../components/layout';
-import { Link } from 'gatsby';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-const letterURL = 'https://justfix.app.law/?i=docassemble.playground1:BrQm8N3wh4C8FPDk.yml&reset=1&key=J2NsHXy22cyUyMoTrHq1nujrX';
+const letterURL = 'https://justfix.app.law/interview?i=docassemble.playground1%3ABrQm8N3wh4C8FPDk.yml#page2';
 
 const CityPage = (props) => (
     <Layout>
         <section className="hero is-fullheight">
             <div className="hero-body">
-                <div className="container has-text-centered">
-                    <h1 className="title has-text-black is-size-1 is-spaced is-uppercase has-text-weight-bold">
+                <div className="container">
+                    <h1 className="title has-text-black is-size-2 is-spaced is-uppercase has-text-weight-bold">
                         {props.content.title}
                     </h1>
-                    <figure className="image is-2by1 rent-logo">
+                    <figure className="image is-3by1 rent-logo">
                         <img src={RentLogo} alt="rent-logo" />
                     </figure>
                     <div className="subtitle has-text-black is-size-5" >
-                        You're not alone. Millions of Americans won't be able to pay rent because of COVID-19
-                    </div>
-                    <div className="subtitle has-text-black is-size-5">
-                        Take action by writing a letter to your landlord
+                        {documentToReactComponents(props.content.description.json)}
                     </div>
                         <br />
                     <a className="button is-info is-large is-uppercase" href={letterURL}> 
-                        Start my letter
+                        {props.content.startLetterCta}
                     </a>
+                    <div className="is-divider" />
+                    <div className="subtitle has-text-black is-size-5" >
+                        {documentToReactComponents(props.content.aboutText.json)}
+                    </div>
                 </div>
             </div>
-            <div className="container footer-link has-text-centered">
-                <Link to='/about'>Who's behind this?</Link>
-            </div>
+        
         </section>
     </Layout>
 )
